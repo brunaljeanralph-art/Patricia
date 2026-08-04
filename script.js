2c122195d3ba632e70e0293b2fa094b16c7b9e5a
Jeanralph Brunal <brunaljeanralph@gmail.com>
2026-08-04 02:52:58 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded",function(){
+
+document.body.style.opacity="0";
+
+setTimeout(function(){
+document.body.style.transition="opacity 2s";
+document.body.style.opacity="1";
+},300);
+
+});
