aae58b4b7b7425da2046f34bded82e9db51422db
Jeanralph Brunal <brunaljeanralph@gmail.com>
2026-09-12 16:48:57 -0400

    Update script.js

--- script.js
+++ script.js
@@ -87,14 +87,3 @@ document.addEventListener("DOMContentLoaded", () => {
 
 });
 
-Sa vle di pa gen okenn lòt "bgMusic" ki pral kreye. "souvenirs.js" ap itilize foto yo sèlman. Mizik la ap rete menm mizik ki te kòmanse nan eksperyans/lettre a. Lè souvenir yo ap pase, li pa restart mizik la.
-
-Epi mwen ajoute "play" ak "pause" listeners yo pou bouton mizik la toujou montre bon eta si mizik la chanje pa yon lòt aksyon. Se evènman natif HTMLMediaElement yo pou sa.
-
-Donk pa mete kòd 42 foto yo nan "script.js". Se "souvenirs.js" ki pral vini apre li nan "index.html"
-
-<script src="script.js"></script>
-<script src="experience.js"></script>
-<script src="souvenirs.js"></script>
-
-Sa se fason mwen ta fè l pou pa gen konfli ant lettre a, particle experience lan, mizik la, ak 42 souvenirs yo.
